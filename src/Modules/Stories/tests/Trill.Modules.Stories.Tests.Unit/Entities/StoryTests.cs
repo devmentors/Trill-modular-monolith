ff51b95dae3f1efb79c001584654054eb0890077
Piotr Gankiewicz <piotr.gankiewicz@gmail.com>
2021-04-26 13:16:10 +0200

    unit test

---
+++ src/Modules/Stories/tests/Trill.Modules.Stories.Tests.Unit/Entities/StoryTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Shouldly;
+using Trill.Modules.Stories.Core.Entities;
+using Trill.Modules.Stories.Core.ValueObjects;
+using Xunit;
+
+namespace Trill.Modules.Stories.Tests.Unit.Entities
+{
+    public class StoryTests
+    {
+        [Fact]
+        public void story_should_be_created_given_valid_data()
+        {
+            // Arrange
+            var id = 1;
+            var title = "Test";
+
+            // Act
+            var story = Create(id, title);
+
+            // Assert
+            story.Id.ShouldBe(id);
+            story.Title.ShouldBe(title);
+        }
+
+        private static Story Create(long id = 1, string title = "Test")
+        {
+            var author = Author.Create(Guid.NewGuid(), "Test");
+            var text = "Lorem ipsum";
+            var tags = new[] {"tag1", "tag2"};
+            var createdAt = DateTime.UtcNow;
+
+            return Story.Create(id, author, title, text, tags, createdAt);
+        }
+    }
+}
